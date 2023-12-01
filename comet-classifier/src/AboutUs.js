ce55ff2de67b803a0f5a56e1fdb75341256cb32b
Rachel Hu <rachelehu@gmail.com>
2023-11-30 22:54:38 -0800

    Develop frontend design for Classifier

--- comet-classifier/src/AboutUs.js
+++ comet-classifier/src/AboutUs.js
@@ -10,7 +10,7 @@ function AboutUs() {
       <img src={aboutUsImage} alt="About Us" />
       </div>
       <p>Our mission is to provide intuitive and accurate classification tools for astronomical bodies.
-        Our dedicated team of astronomers, engineers, and data scientists has worked tirelessly to bring you state-of-the-art technology for comet classification and analysis.
+        Our dedicated team of engineers and data scientists from the University of California, Berkeley has worked tirelessly to bring you state-of-the-art technology for comet classification and analysis.
         Through our website, we aim to make it easy for professionals and enthusiasts alike to access high-quality data and insights about comets.
         Whether you're conducting research or just have a passion for astronomy, Comet Classifier is here to serve as your reliable resource.
         Thank you for visiting, we hope you find the tools and information you need for your celestial explorations!</p>
--- comet-classifier/src/Model.js
+++ comet-classifier/src/Model.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Model.css';
 
-
 function Model() {
+  // State to manage the visibility of the comet text
+  const [isCometVisible, setCometVisible] = useState(false);
+
+  // Function to handle button click and toggle comet text visibility
+  const showCometText = () => {
+    setCometVisible(true);
+  };
+
   return (
     <div className="model">
       <h2>Comet Classifier</h2>
-      {/* Content goes here */}
+      <div className="content">
+        <div className="input-section">
+          <h3>Input</h3>
+          <input type="text" placeholder="eccentricity" />
+          <input type="text" placeholder="sidereal orbital period" />
+          <input type="text" placeholder="data arc length" />
+          {/* Button to reveal the comet text */}
+          <button onClick={showCometText}>Classify</button>
+        </div>
+        <div className="output-section">
+          <h3>Output</h3>
+          {/* Paragraph text that is conditionally rendered based on isCometVisible */}
+          {isCometVisible && <p>This is a comet! ☄️</p>}
+        </div>
+      </div>
     </div>
   );
 }
